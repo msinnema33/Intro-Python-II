7ba98d48b68f0682b9fa88ca80e5f4f6f5253f83
Mike Sinnema <msinnema33@gmail.com>
2020-06-04 12:31:46 -0700

    updated room

--- src/room.py
+++ src/room.py
@@ -1,2 +1,24 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.items = []
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def print_description(self):
+        return f'{self.description}'
+
+    def list_items(self):
+        if not self.items:
+            print('This room has no items')
+        else:
+            print('This room has the following items: ')
+            for item in self.items:
+                print(item.name)
+                print(item.description)
+        
